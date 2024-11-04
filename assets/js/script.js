04fedef037ff1ee190625d3712e51a33908dd65d
Joseph Pal <josephpal27@gmail.com>
2024-11-04 12:50:35 +0530

    home page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,26 @@ footIconBox.forEach((box, i) => {
 })
 
 // ----------------------------------------------------------------------------------------------------------------------------------------
+
+// Functionality For Back to Top Btn
+let backToTopDiv = document.querySelector(".back-to-top");
+let backToTopBtn = document.querySelector(".back-to-top .fa-arrow-up");
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    // Adjust the pixel value as needed
+    backToTopDiv.classList.add("show");
+  } else {
+    backToTopDiv.classList.remove("show");
+  }
+});
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------
